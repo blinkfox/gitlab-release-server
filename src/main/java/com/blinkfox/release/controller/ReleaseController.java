01cfb72e1ac36b3018d77280bc8fa6f5fd7aa3dd
blinkfox <chenjiayin1990@163.com>
2019-06-21 23:43:04 +0800

    重构了IndexController 接口的发布功能到ReleaseController 中

--- src/main/java/com/blinkfox/release/controller/ReleaseController.java
+++ src/main/java/com/blinkfox/release/controller/ReleaseController.java
@@ -8,35 +8,33 @@ import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 首页根控制器.
+ * 发布 release 的控制器类.
  *
- * @author blinkfox on 2019-06-14.
+ * @author blinkfox on 2019-06-21.
  */
 @Slf4j
 @CrossOrigin
-@Controller
-@RequestMapping
-public class IndexController {
+@RestController
+@RequestMapping("/releases")
+public class ReleaseController {
 
     /**
      * index.html 首页请求.
      *
      * @return 字符串
      */
-    @GetMapping
-    public ModelAndView index(ModelAndView modelView) {
-        modelView.setViewName("index");
-        return modelView;
+    @PostMapping("/")
+    public ResponseEntity<Map<String, String>> release(@RequestBody ReleaseInfo releaseInfo) {
+        log.info("release: {}", releaseInfo);
+        Map<String, String> map = new HashMap<>(8);
+        return ResponseEntity.ok(map);
     }
 
 }
